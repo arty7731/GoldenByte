226ab550bf69b9f3f7aebb01a57fd1f48c8e7777
Artem <arty7731@gmail.com>
2016-06-23 11:18:42 +0300

    Add scroll and angle limit

--- UnityGoldenByte/Assets/Scripts/InputControllers/MouseCamConstructorControles.cs
+++ UnityGoldenByte/Assets/Scripts/InputControllers/MouseCamConstructorControles.cs
@@ -1,57 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseCamConstructorControles : MonoBehaviour {
+public class MouseCamConstructorControles : MonoBehaviour
+{
+	[SerializeField]
+	GameObject lookGameObject;
+	[SerializeField]
+	float distance = 10.0F;
+	[SerializeField]
+	float sensitivity = 0.5F;
 
+	[SerializeField]
+	float sSensitivity = 1F;
+	[SerializeField]
+	float minDistance = 2F;
+	[SerializeField]
+	float maxDistance = 20F;
 
-	public GameObject TrackedGameObject;
-	public float sensitivity = 0.5F;
-	private Camera LookCamera;
-	private Vector3 MousePosStart;
-	private float MyAngle = 0F;
+	[SerializeField]
+	float yMinLimit = -20F;
+	[SerializeField]
+	float yMaxLimit = 80F;
 
-	// Use this for initialization
-	void Start ()
+	float x = 0.0F;
+	float y = 0.0F;
+
+void Start()
 	{
-		LookCamera = gameObject.GetComponent<Camera>();
+		var angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+	void LateUpdate()
 	{
-		MousePosStart = Input.mousePosition;
+		distance -= (Input.GetAxis("Mouse ScrollWheel") * sSensitivity * 2);
+		if (distance < minDistance) distance = minDistance;
+		if (distance > maxDistance) distance = maxDistance;
+		var rotation = Quaternion.Euler(y, x, 0);
+		var position = rotation * new Vector3(0.0F, 0.0F, -distance) + lookGameObject.transform.position;
+
+		transform.rotation = rotation;
+		transform.position = position;
+
+		if (Input.GetMouseButton(0))
+		{
+			x += Input.GetAxis("Mouse X") * (sensitivity * 10);
+			y -= Input.GetAxis("Mouse Y") * (sensitivity * 10);
+
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+			transform.rotation = rotation;
+			transform.position = position;
+		}
 	}
 
-	void FixedUpdate()
+	static float ClampAngle(float angle, float min, float max)
 	{
-		if (Input.GetMouseButton(0) && MousePosStart != Input.mousePosition)
-		{
-			MyAngle = 0;
-
-			MyAngle = sensitivity * (Input.mousePosition.x - MousePosStart.x);
-			var rotationCamera = LookCamera.transform.rotation;
-			LookCamera.transform.RotateAround(TrackedGameObject.transform.position, LookCamera.transform.up, MyAngle);
-			
-			MyAngle = sensitivity * (Input.mousePosition.y - MousePosStart.y);
-			LookCamera.transform.RotateAround(TrackedGameObject.transform.position, LookCamera.transform.right, -MyAngle);
-
-			//var rotationCam = LookCamera.transform.rotation;
-			//if (rotationCam.x > 90)
-			//{
-			//	LookCamera.transform.rotation = Quaternion.Euler(90, LookCamera.transform.rotation.y, LookCamera.transform.rotation.z);
-			//}
-			//else if (rotationCam.x < 0)
-			//{
-			//	LookCamera.transform.rotation = Quaternion.Euler(0, LookCamera.transform.rotation.y, LookCamera.transform.rotation.z);
-
-			//}
-		}
-
-		var scrollMove = Input.GetAxis("Mouse ScrollWheel");
-		if (scrollMove != 0)
-		{
-			LookCamera.transform.position = new Vector3(LookCamera.transform.position.x, LookCamera.transform.position.y, LookCamera.transform.position.z + scrollMove * 10);
-		}
-		
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle, min, max);
 	}
 }
